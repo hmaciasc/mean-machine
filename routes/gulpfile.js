de1a672c2cae9ed165247bc48771fd2d70cf637a
Héctor Macías <hectormaciasc@gmail.com>
2016-04-30 17:18:22 +0100

    routes project
    ended

---
+++ routes/gulpfile.js
@@ -0,0 +1,63 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var minifyCSS = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var nodemon = require('gulp-nodemon');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var ngAnnotate = require('gulp-ng-annotate');
+
+gulp.task('css', function() {
+    return gulp.src('public/assets/css/style.less')
+        .pipe(less())
+        .pipe(minifyCSS())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('public/assets/css'));
+});
+
+gulp.task('js', function() {
+    return gulp.src(['server.js', 'public/app/*.js', 'public/app/**/*.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src(['public/app/*.js', 'public/app/**/*.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(concat('all.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/dist'));
+});
+
+gulp.task('angular', function() {
+    return gulp.src(['public/app/*.js', 'public/app/**/*.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(ngAnnotate())
+        .pipe(concat('app.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/dist'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('public/assets/css/style.less', ['css']);
+    gulp.watch(['server.js', 'public/app/*.js', 'public/app/**/*.js'], ['js', 'angular']);
+});
+
+gulp.task('nodemon', function() {
+    nodemon({
+            script: 'server.js',
+            ext: 'js less html'
+        })
+        .on('start', ['watch'])
+        .on('change', ['watch'])
+        .on('restart', function() {
+            console.log('Restarted!');
+        });
+});
+
+gulp.task('default', ['nodemon']);
--- routes/server.js
+++ routes/server.js
@@ -8,12 +8,27 @@ app.get("/", function(req, res) {
 
 var adminRouter = express.Router();
 
+adminRouter.use(function(req, res, next) {
+    console.log(req.method, req.url);
+    next();
+});
+
 adminRouter.get("/", function(req, res) {
     res.send("I am dashboard!");
 });
 
 adminRouter.get("/users", function(req, res) {
     res.send("I show all the users!");
+});
+
+adminRouter.param("name", function(req, res, next, name) {
+    console.log("Validating " + name);
+    req.name = name;
+    next();
+});
+
+adminRouter.get("/users/:name", function(req, res) {
+    res.send("Hello " + req.params.name + "!");
 });
 
 adminRouter.get("/posts", function(req, res) {
