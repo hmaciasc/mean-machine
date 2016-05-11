e95071bc9829ddee6407c5bb7508fd624cf87736
Héctor Macías <hectormaciasc@gmail.com>
2016-05-11 12:50:58 +0100

    users
    user routes for / get post put implemented

---
+++ api/routers/apiRouter.js
@@ -0,0 +1,64 @@
+var express = require("express"),
+    mongoose = require("mongoose"),
+    User = require("../app/models/user.js");
+
+mongoose.connect("mongodb://mm:mm123@ds017582.mlab.com:17582/mdeftbit");
+
+var apiRouter = express.Router();
+
+apiRouter.use(function(req, res, next) {
+    console.log("Somebody just came to our app!");
+    next();
+});
+
+apiRouter.get("/", function(req, res) {
+    res.json({
+        message: "hooray! welcome"
+    });
+});
+
+apiRouter.use("/api", apiRouter);
+
+apiRouter.route("/users").post(function(req, res){
+  var user = new User();
+  user.name = req.body.name;
+  user.username = req.body.username;
+  user.password = req.body.password;
+  user.save(function(err){
+  if(err){
+    if(err.code == 11000)
+      return res.json({success: false, message: "A user with that username already exists. "});
+    else
+      return res.send(err);
+  }
+    res.json({message: "User created!"});
+  });
+})
+.get(function(req, res){
+  User.find(function(err, users){
+    if(err) res.send(err);
+    res.json(users);
+  });
+});
+
+
+apiRouter.route("/users/:user_id")
+    .get(function(req, res) {
+        User.findById(req.params.user_id, function(err, user) {
+            if (err) res.send(err);
+            res.json(user);
+        });
+    })
+    .put(function(req, res) {
+        User.findById(req.params.user_id, function(err, user) {
+            if (err) res.send(err);
+            if (req.body.name) user.name = req.body.name;
+            if (req.body.username) user.username = req.body.username;
+            if (req.body.password) user.password = req.body.password;      
+            user.save(function(err) {
+                if (err) res.send(err);res.json({ message: "User updated!" });
+            })      
+        })
+    });
+
+module.exports = apiRouter;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var bodyParser = require('body-parser');
-var morgan = require('morgan');
-var mongoose = require('mongoose');
+var bodyParser = require("body-parser");
+var morgan = require("morgan");
+var mongoose = require("mongoose");
 var port = process.env.PORT || 8080;
+var User = require("./app/models/user");
+var apiRouter = require("./routers/apiRouter.js");
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -11,27 +13,19 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Origin', 'GET, POST');
-    res.setHeader('Access-Control-Allow-Origin', 'X-Requested-With. conten-type, Authorization');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "GET, POST");
+    res.setHeader("Access-Control-Allow-Origin", "X-Requested-With. conten-type, Authorization");
     next();
 });
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get('/', function(req, res) {
-    res.send('Welcome to the home page!');
+app.get("/", function(req, res) {
+    res.send("Welcome to the home page!");
 });
 
-var apiRouter = express.Router();
-
-apiRouter.get('/', function(req, res) {
-    res.json({
-        message: 'hooray! welcome to our api!'
-    });
-});
-
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
 app.listen(port);
-console.log('Magic happens on port' + port);
+console.log("Magic happens on port" + port);
